13f101b580e910d35fad50cb5830830bcbee7567
SHklyaev-AA <unforgiven.91@mail.ru>
2020-07-27 14:52:28 +0400

    Фиксация

---
+++ lesson1/app.js
@@ -0,0 +1,17 @@
+'use strict';
+//урок 1
+
+//Задание 1
+let tc = prompt("ВВедите температуру по Цельсию ");
+let tf = (9 / 5) * tc + 32;
+alert(`Температура по Фаренгейту: ${tf}.`);
+
+//Задание 2
+let name = "Василий";
+let admin;
+admin = name;
+console.log(admin);
+
+//Задание 4
+let a=1000+"108"//1000108
+console.log(a);
